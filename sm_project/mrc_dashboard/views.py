14dad6a5ce146cf6a3c482806dd587ed60f6b264
David Gillies <david.gillies.rhu@gmail.com>
2015-01-22 16:33:54 +0000

    decided to do the apps list just using hard coding

--- sm_project/mrc_dashboard/views.py
+++ sm_project/mrc_dashboard/views.py
@@ -4,10 +4,10 @@ from django.apps import apps
 from sm_project.settings import base as settings
 
 # models_list = ','.join([x.__name__ for x in apps.get_models()])
-apps_list = ', '.join(settings.MY_APPS)
+# apps_list = ', '.join(settings.MY_APPS)
 
 def index(request):
     return HttpResponse("""<!doctype html><html><head><title>Dashboard</title></head>
-    """+apps_list+'</html>')
+    </html>""")
 
 # Create your views here.
