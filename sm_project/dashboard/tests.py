2a63211523162651d649230cc3c5dfc886b97f52
David Gillies <david.gillies.rhu@gmail.com>
2015-01-22 14:20:03 +0000

    first dashboard unittests

--- sm_project/dashboard/tests.py
+++ sm_project/dashboard/tests.py
@@ -1,3 +1,18 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from dashboard.views import index
+from django.http import HttpRequest
 
-# Create your tests here.
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_index(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_dashboard_returns_html(self):
+        request = HttpRequest()
+        response = index(request)
+        self.assertTrue(response.content.startswith(b'<!doctype html>'))
+        self.assertIn(b'<title>Dashboard</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
