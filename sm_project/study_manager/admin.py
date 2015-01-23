b4422f02fcdaf67c3034686a0ea22bf4f0941493
David Gillies <david.gillies.rhu@gmail.com>
2015-01-23 15:51:38 +0000

    added models to admin

--- sm_project/study_manager/admin.py
+++ sm_project/study_manager/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class VolunteerAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(Volunteer, VolunteerAdmin)
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(Appointment, AppointmentAdmin)
+
+
+class SurgeryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(Surgery, SurgeryAdmin)
+
+
+class GPAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        obj.save()
+
+admin.site.register(GP, GPAdmin)
